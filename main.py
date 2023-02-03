3ed7aa1e4cc89f6b99e41e07da4a191b11c57711
theHaruspex <75146768+theHaruspex@users.noreply.github.com>
2023-02-03 11:25:34 -0800

    Added download functionality.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from os.path import basename
 
 
 def get_page_soup(url):
@@ -10,23 +11,37 @@ def get_page_soup(url):
 def get_chapter_links():
     toc_url = "https://read-vinlandsaga.com/"
     soup = get_page_soup(toc_url)
-    nav_elements = [str(element) for element in soup.findAll(attrs={"class": "su-post"})]
+    nav_elements = soup.find_all("li", {"class": "su-post"})
     chapter_links = []
     for element in nav_elements:
-        beginning_index = element.index("https")
-        ending_index = element.index('/">')
-        link = element[beginning_index:ending_index]
+        a_element = element.find("a")
+        link = a_element["href"]
         if link not in chapter_links:
             chapter_links.append(link)
     chapter_links.reverse()
     return chapter_links
 
-def get_image_links(chapter_url):
+def get_image_urls(chapter_url):
     soup = get_page_soup(chapter_url)
+    nav_elements = soup.findAll("img", {"class": "aligncenter"})
+    image_links = []
+    for element in nav_elements:
+        img_url = element['src']
+        if "cdn.hxmanga.com" in img_url:
+            image_links.append(img_url)
+    return image_links
+
+
+def download_image(url, folderpath):
+    filepath = f"{folderpath}/{basename(url)}"
+    with open(filepath, "wb") as file:
+        file.write(requests.get(url).content)
+
 
 
 
 
 vinland_links = get_chapter_links()
-image_links = get_image_links(vinland_links[0])
-print(image_links)
+image_urls = get_image_urls(vinland_links[0])
+for url in image_urls:
+    download_image(url, "Chapter 1")
